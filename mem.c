04e565801e07a9537df9672e83ac20340810ad77
green <25813+artgreen@users.noreply.github.com>
2024-08-27 19:37:21 -0500

    add ai generated docs

--- mem.c
+++ mem.c
@@ -1,8 +1,44 @@
 #include "mem.h"
 
+/**
+ * @brief The memory address space.
+ *
+ * This variable represents the memory address space. It is an array of bytes
+ * with a size of MEM_MAX. The memory address space is where data can be read
+ * from and written to during program execution.
+ *
+ * @note The memory address space is defined in mem.h and has a size of MEM_MAX.
+ *       MEM_MAX is calculated as (4 * 1024 * 1024) - 1.
+ *
+ * @see mem.h
+ */
 byte addressSpace[MEM_MAX];
+
+/**
+ * @brief Indicates if I/O operations are enabled.
+ *
+ * The variable "no_io" is an integer that indicates whether I/O operations are enabled or disabled.
+ * When "no_io" is set to 0, I/O operations are enabled. When "no_io" is set to a non-zero value,
+ * I/O operations are disabled.
+ *
+ * @note The variable "no_io" is initially set to 0 in the "debugger.c" file but can be changed in other parts of the program.
+ *
+ * @see debugger.c
+ */
 int no_io;
 
+/**
+ * @brief Reads a byte from standard input in a blocking manner.
+ *
+ * This function reads a byte from standard input and returns it as a byte value.
+ * It uses the getchar() function to read the byte.
+ *
+ * @return The byte read from standard input.
+ *
+ * @note This function assumes that the byte read is within the range of valid byte values.
+ *       If the reading operation encounters an error, the behavior is dependent on the
+ *       implementation of the getchar() function.
+ */
 byte BlockingRead(void)
 {
     return (byte)(getchar());
@@ -14,10 +50,39 @@ void Emit(byte b)
     fflush(stdout);
 }
 
+/**
+ * @brief Returns a pointer to the base of the memory address space.
+ *
+ * This function returns a pointer to the base of the memory address space by
+ * returning the address of the first element of the "addressSpace" array.
+ *
+ * @return A pointer to the base of the memory address space.
+ *
+ * @see addressSpace[]
+ */
 byte *MEM_base(void)
 {
     return &addressSpace[0];
 }
+
+/**
+ * @brief Reads a byte from memory at the given address.
+ *
+ * This function reads a byte from memory at the specified address.
+ * The address is first masked to ensure it is within the valid memory range.
+ *
+ * @param address The address to read from.
+ * @param timestamp The timestamp value.
+ * @param emulFlags The emulation flags.
+ * @return The byte value at the specified address.
+ *
+ * @note The address is masked to ensure it is within the valid memory range.
+ *       If the address is greater than MEM_MAX, 0 is returned.
+ *
+ * @note If the address is 0x000001 and the no_io flag is not set, it returns the result of the BlockingRead function.
+ *
+ * @see BlockingRead()
+ */
 byte MEM_readMem(word32 address, word32 timestamp, word32 emulFlags)
 {
     address &= 0xFFFFFF;
@@ -32,6 +97,19 @@ byte MEM_readMem(word32 address, word32 timestamp, word32 emulFlags)
     return addressSpace[address];
 }
 
+/**
+ * @brief Writes a byte to memory at the given address.
+ *
+ * This function writes a byte to memory at the specified address.
+ *
+ * @param address The address to write to.
+ * @param b The byte value to write.
+ * @param timestamp The timestamp value.
+ *
+ * @note If the address is 0x000000, the function exits if the byte value is 0. Otherwise, it calls the Emit function.
+ *
+ * @see Emit()
+ */
 void MEM_writeMem(word32 address, byte b, word32 timestamp)
 {
     if (address == 0x000000)
