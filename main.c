ad62d26105445884d99622693d0e49ab5ed6f6ee
green <25813+artgreen@users.noreply.github.com>
2024-08-27 19:16:49 -0500

    moved file stuff into load_program

--- main.c
+++ main.c
@@ -18,37 +18,50 @@ void EMUL_hardwareUpdate(word32 timestamp)
 {
 }
 
-int main(int argc, char** argv)
+int load_program(const char *filename, const word32 code_start)
 {
-    FILE* f = 0;
-
-    CPUEvent_initialize(); // now needed
-
-    if (argc >= 2)
-        f = fopen(argv[1], "r");
-
+    FILE* f = fopen(filename, "r");
     if (!f)
     {
-        fprintf(stderr, "Could not open <%s>\n", argv[1]);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Could not open <%s>\n", filename);
+        return 0;
     }
-
-    fread(&addressSpace[CODE_START], sizeof(addressSpace), 1, f);
+    fread(&addressSpace[code_start], sizeof(addressSpace), 1, f);
     fclose(f);
+    return 1;
+}
 
+void run(void)
+{
     no_io = 0;
     CPU_reset();
     addressSpace[0xfffc] = CODE_START & 0xFF;
     addressSpace[0xfffd] = (CODE_START >> 8) & 0xFF;
-
-    if (argc >= 3 && !strcmp("dbg", argv[2]))
-        CPU_setTrace(1);
-    if (argc >= 3 && !strcmp("trace", argv[2]))
-    {
-        CPU_setTrace(1);
-        trace = 1;
-    }
     CPU_run();
+}
 
+int main(int argc, char** argv)
+{
+    CPUEvent_initialize(); // now needed
+
+    if (argc >= 2)
+    {
+        if (load_program(argv[1], CODE_START))
+        {
+            if (argc >= 3 && !strcmp("dbg", argv[2]))
+                CPU_setTrace(1);
+            if (argc >= 3 && !strcmp("trace", argv[2]))
+            {
+                CPU_setTrace(1);
+                trace = 1;
+            }
+            run();
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Usage: %s <filename> [options]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
     return 0;
 }
